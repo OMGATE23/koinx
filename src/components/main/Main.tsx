d955aac97a5620d8efce25bfe6a20043baa3c711
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-06 22:08:31 +0530

    feat : main page component
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import BreadCrumb from "./BreadCrumb";
+import CoinOverview from "./CoinOverview";
+import BannerComponent from "./BannerComponent";
+import TrendingCoins from "./TrendingCoins";
+import YouMayAlsoLike from "./YouMayAlsoLike";
+
+const Main = () => {
+  return (
+    <div>
+      <BreadCrumb />
+      <div className="mx-4 md:mx-12 mb-12 flex flex-col md:flex-row justify-center items-center md:justify-between md:items-start gap-12">
+        <CoinOverview />
+        <div className="w-[30%] md:flex hidden flex-col justify-center gap-8">
+          <BannerComponent />
+          <TrendingCoins />
+        </div>
+      </div>
+      <YouMayAlsoLike />
+      <div className="w-full  md:hidden flex flex-col justify-center gap-8">
+        <BannerComponent />
+        <TrendingCoins />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
