7e6ff5f824b2f71f25450a7aa63ba45d64dd1dfb
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-06 23:18:16 +0530

    style : made banner component and trending sticky
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,9 +9,9 @@ const Main = () => {
   return (
     <div>
       <BreadCrumb />
-      <div className="mx-4 md:mx-12 mb-12 flex flex-col md:flex-row justify-center items-center md:justify-between md:items-start gap-12">
+      <div className="mx-4 relative md:mx-12 mb-12 flex flex-col md:flex-row justify-center items-center md:justify-between md:items-start gap-12">
         <CoinOverview />
-        <div className="w-[30%] md:flex hidden flex-col justify-center gap-8">
+        <div className="w-[30%] sticky top-8 md:flex hidden flex-col justify-center gap-8">
           <BannerComponent />
           <TrendingCoins />
         </div>
