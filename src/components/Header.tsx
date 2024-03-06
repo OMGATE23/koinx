f90515b0418f3703e15c24de3740c6d4a4554ce5
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-06 22:04:05 +0530

    feat : Header component
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between bg-white items-center px-6 md:px-12 border-b-[1px] border-[#DEDFE2]">
+      <Link href="/">
+        <img src="/assets/logo.svg" />
+      </Link>
+      <div className="hidden md:flex justify-evenly font-[500] items-center gap-8">
+        <Link href="/">Crypto Taxes</Link>
+        <Link href="/">Free Tools</Link>
+        <Link href="/">Resource Center</Link>
+        <Link
+          className="ml-2 px-4 py-2 text-white rounded-lg header-link-bg"
+          href="/"
+        >
+          Get Started
+        </Link>
+      </div>
+      <div>
+        <button>
+          <img src="/assets/hamburger.svg" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
