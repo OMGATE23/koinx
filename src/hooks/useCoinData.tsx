a94fceea86cd546d397a5d2bda5294b4651765ec
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-06 22:08:14 +0530

    feat : hook to fetch coin price data
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/hooks/useCoinData.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export type CoinData = {
+  inr: number;
+  coinName: string;
+  inr_24h_change: number;
+  usd: number;
+  usd_24h_change: number;
+};
+
+const useCoinData = (coin: string = "bitcoin") => {
+  let params = useParams();
+  coin = (params.coin as string) || coin;
+  const [coinData, setCoinData] = useState<CoinData>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>();
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`/api/coin?coin=${coin}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const coinName = Object.keys(data)[0];
+        const coinData = { coinName, ...data[coinName] };
+        setCoinData(coinData);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [coin]);
+
+  return { coinData, loading, error };
+};
+
+export default useCoinData;
