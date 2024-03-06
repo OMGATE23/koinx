5233408d71020d8d7c97933e693a77888b46c020
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-06 22:08:23 +0530

    feat : hook for coin information
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/hooks/useCoinInfo.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export type CoinInfo = {
+  symbol: string;
+  image: {
+    small: string;
+  };
+};
+
+const useCoinInfo = (coin: string = "bitcoin") => {
+  let params = useParams();
+  coin = (params.coin as string) || coin;
+  const [coinInfo, setCoinInfo] = useState<CoinInfo | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>();
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`/api/coinInfo?coin=${coin}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCoinInfo(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [coin]);
+
+  return { coinInfo, loading, error };
+};
+
+export default useCoinInfo;
