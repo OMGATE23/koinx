30c728c0a8dae15b1d511dc05c59f0622cfcbebd
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-06 22:02:22 +0530

    first commit : app init
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Main from "@/components/main/Main";
+
+export default function Home() {
+  return (
+    <div className="bg-[#EFF2F5]">
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
